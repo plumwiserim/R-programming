968d3ba592e461007926ce7716b910daae3f21bb
LEE <dulfla_5@naver.com>
2022-01-27 21:54:32 +0900

    create chapter03-2

---
+++ chapter03-3.R
@@ -0,0 +1,19 @@
+# 변수 만들기기
+x <- c(1, 2, 3)
+x
+#함수 적용하기 
+mean(x)
+
+max(x)
+min(x)
+
+str5
+paste(str5, collapse = ",")
+
+paste(str5, collapse = " ")
+
+x_mean <- mean(x)
+x_mean
+
+str5_paste <- paste(str5, collapse = " ")
+str5_paste
