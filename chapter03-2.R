f433da8cb4920928bc067040df87c73d18f4f11c
LEE <dulfla_5@naver.com>
2022-01-27 21:51:09 +0900

    create chapter03-2

---
+++ chapter03-2.R
@@ -0,0 +1,51 @@
+a <- 1 # a에 1 할당당
+a
+
+b <- 2
+b
+c <- 3
+c
+d <- 3.5
+d
+
+a+b
+a+b+c
+4/b
+5*b
+
+var1 <- c(1, 2, 5, 7, 8) # 숫자 5개로 구성된 var1 생성
+var1
+
+var2 <- c(1:5) # 1~5까지 연속 값으로 var2 생성
+var2
+
+var3 <- seq(1, 5) # 1~5까지 연속 값으로 var3 생성
+var3
+
+var4 <- seq(1, 10, by = 2) # 1~10까지 2 간격 연속 값으로 var4 생성
+var4
+
+var5 <- seq(1, 10, by = 3) # 1~10까지 3 간격 연속 값으로 var5 생성성
+var5
+
+var1
+var1+2
+
+var1
+var2
+var1+var2
+
+str1 <- "a"
+str1
+
+str2 <- "text"
+str2
+str3 <- "Hello World!"
+str3
+
+str4 <- c("a", "b", "c")
+str4
+str5 <- c("Hello!", "World", "is", "good!")
+str5
+
+str1+2
