ee4a38107e637e52b9ba406a94fa047c4f418839
LEE <dulfla_5@naver.com>
2022-01-28 12:33:15 +0900

    create chapter05-2

---
+++ chapter05-2.R
@@ -0,0 +1,15 @@
+df_raw <- data.frame(var1 = c(1, 2, 1), 
+                     var2 = c(2, 3, 2))
+df_raw
+
+install.packages("dplyr") # dplyr 설치
+library(dplyr) # dplyr 로드
+
+df_new <- df_raw # 복사본 생성
+df_new
+
+df_new <- rename(df_new, v2 = var2)
+df_new
+
+df_raw
+df_new
