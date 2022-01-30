21eba32ad4451cbe96ca6b279ae52f6d881db9a9
LEE <dulfla_5@naver.com>
2022-01-30 16:34:05 +0900

    create chapter09-7

---
+++ chapter09-7.R
@@ -0,0 +1,27 @@
+# 남성 직업 빈도 상위 10개 추출
+job_male <- welfare %>% 
+  filter(!is.na(job) & sex == "male") %>% 
+  group_by(job) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n)) %>% 
+  head(10)
+job_male
+
+# 여성 직업 빈도 상위 10개 추출
+job_female <- welfare %>% 
+  filter(!is.na(job) & sex == "female") %>% 
+  group_by(job) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n)) %>% 
+  head(10)
+job_female
+
+# 남성 직업 빈도 상위 10개 직업
+ggplot(data = job_male, aes(x = reorder(job, n), y = n)) +
+  geom_col() +
+  coord_flip()
+
+# 여성 직업 빈도 상위 10개 직업
+ggplot(data = job_female, aes(x = reorder(job, n), y = n)) +
+  geom_col() +
+  coord_flip()
