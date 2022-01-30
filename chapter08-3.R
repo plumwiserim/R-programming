1a5a4e1ec8deac0f24e4a6d4c5e6c71ee2260499
LEE <dulfla_5@naver.com>
2022-01-30 16:32:19 +0900

    create chapter08-3

---
+++ chapter08-3.R
@@ -0,0 +1,31 @@
+library(dplyr)
+df_mpg <- mpg %>% 
+  group_by(drv) %>% 
+  summarise(mean_hwy = mean(hwy))
+df_mpg
+
+ggplot(data = df_mpg, aes(x = drv, y = mean_hwy)) + geom_col()
+
+ggplot(data = df_mpg, aes(x = reorder(drv, -mean_hwy), y = mean_hwy)) + geom_col()
+
+ggplot(data = mpg, aes(x = drv)) + geom_bar()
+
+ggplot(data = mpg, aes(x = hwy)) + geom_bar()
+
+# 혼자서 해보기
+
+# mpg 데이터 불러오기
+mpg <- as.data.frame(ggplot2::mpg) 
+
+# 평균 표 생성
+mpg_suv <- mpg %>%
+  filter(class == "suv") %>% 
+  group_by(manufacturer) %>% 
+  summarise(mean_cty = mean(cty)) %>% 
+  arrange(desc(mean_cty)) %>% 
+  head(5)
+
+# 그래프 생성
+ggplot(data = mpg_suv, aes(x = reorder(manufacturer, -mean_cty), y = mean_cty)) + geom_col()
+
+ggplot(data = mpg, aes(x = class)) + geom_bar()
