4e46aaa8e1138a347f1ec9952fdfd8a70e7ee935
LEE <dulfla_5@naver.com>
2022-01-30 16:33:41 +0900

    create chapter09-5

---
+++ chapter09-5.R
@@ -0,0 +1,23 @@
+sex_income <- welfare %>% 
+  filter(!is.na(income)) %>% 
+  group_by(ageg, sex) %>% 
+  summarise(mean_income = mean(income))
+sex_income
+
+ggplot(data = sex_income, aes(x = ageg, y = mean_income, fill = sex)) + 
+  geom_col() + 
+  scale_x_discrete(limits = c("young", "middle", "old"))
+
+ggplot(data = sex_income, aes(ageg, y = mean_income, fill = sex)) + 
+  geom_col(position = "dodge") + 
+  scale_x_discrete(limits = c("young", "middle", "old"))
+
+# 성별 연령별 월급 평균표 만들기
+sex_age <- welfare %>% 
+  filter(!is.na(income)) %>% 
+  group_by(age, sex) %>% 
+  summarise(mean_income = mean(income))
+head(sex_age)
+
+# 그래프 만들기
+ggplot(data = sex_age, aes(x = age, y = mean_income, col = sex)) + geom_line()
